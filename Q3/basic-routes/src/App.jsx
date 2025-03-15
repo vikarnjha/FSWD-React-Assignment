e60d4ec4b429ba534f1c102889abccea0883bed1
Vikarn Jha <vikarnjha91@gmail.com>
2025-03-15 17:43:25 +0530

    Question 3 completed successfully

--- Q3/basic-routes/src/App.jsx
+++ Q3/basic-routes/src/App.jsx
@@ -1,13 +1,17 @@
-import {} from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
