65edecf5cc0b617b03e6d9aa7c3ea21113f21469
Vikarn Jha <vikarnjha91@gmail.com>
2025-03-15 22:50:00 +0530

    Question 6 completed

--- Q6/link-navlink/src/App.jsx
+++ Q6/link-navlink/src/App.jsx
@@ -1,13 +1,28 @@
-import { } from 'react'
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import About from "./About";
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <nav>
+        {/* Using Link */}
+        <Link to="/">Home</Link> |  
+        <Link to="/about">About</Link>
+        <br />
+        {/* Using NavLink with Active Styling */}
+        <NavLink to="/" className={({ isActive }) => (isActive ? "active-link" : "")}>
+          Home
+        </NavLink> |  
+        <NavLink to="/about" className={({ isActive }) => (isActive ? "active-link" : "")}>
+          About
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
+export default App;
