3cfca2ce9837f03717dda748b7264bf487959e89
Vikarn Jha <vikarnjha91@gmail.com>
2025-03-15 17:43:59 +0530

    formated App.jsx

--- Q3/basic-routes/src/App.jsx
+++ Q3/basic-routes/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 
-
 function App() {
   return (
     <Router>
